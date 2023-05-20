703567a2dccb40ca0d9f821ca471d3b8bd0d2e6b
samuki <s.kiegeland@gmx.de>
2023-05-21 00:53:22 +0200

    Fix for multiple runs

--- branches_make_polyvol.sh
+++ branches_make_polyvol.sh
@@ -1,3 +1,5 @@
+# Read branches
+IFS=' ' read -r -a desired_branches <<< "$1"
 # Make sure to fetch updates, but don't merge yet
 git remote rm origin
 git remote add origin git@gitlab.inf.ethz.ch:COURSE-ASL/asl23/team29.git
@@ -5,23 +7,30 @@ git fetch --all   # origin is default
 
 # list all branches
 for branch in $(git branch -r | grep -v HEAD); do
-    echo "Checking out $branch"
-    # if the branch is not the same as the remote branch...
-    git checkout ${branch#origin/} &&
-    git pull origin ${branch#origin/}  &&
-    make &&
-    cp polyvol /tmp/${branch#origin/}_polyvol;
-    make clean
+  for desired_branch in "${desired_branches[@]}"; do
+    if [[ "$desired_branch" == ${branch#origin/} ]]; then
+      echo "Checking out $branch"
+      # if the branch is not the same as the remote branch...
+      git checkout ${branch#origin/} &&
+      git pull origin ${branch#origin/}  &&
+      make &&
+      cp polyvol /tmp/${branch#origin/}_polyvol;
+      make clean
+    fi
+  done
 done
 
 # Copy all PolyVol executables to branch
 git checkout benchmarking
 DIRECTORY=executables
-if [ -d "$DIRECTORY" ]; then
-  echo "$DIRECTORY does exist."
-  rm -r $DIRECTORY
+if ! [ -d "$DIRECTORY" ]; then
+  echo "Creating $DIRECTORY"
+  mkdir $DIRECTORY
 fi
-mkdir $DIRECTORY
 for branch in $(git branch -r | grep -v HEAD); do
-    cp /tmp/${branch#origin/}_polyvol $DIRECTORY;
+  for desired_branch in "${desired_branches[@]}"; do
+    if [[ "$desired_branch" == ${branch#origin/} ]]; then
+      cp /tmp/${branch#origin/}_polyvol $DIRECTORY;
+    fi
+  done
 done
