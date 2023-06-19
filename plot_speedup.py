5efb064cfbdb7a8a96dab9202a5a0512553ffa0b
samuki <s.kiegeland@gmx.de>
2023-06-19 20:08:52 +0200

    Update plotting code

--- plot_speedup.py
+++ plot_speedup.py
@@ -1,5 +1,6 @@
 import re
 import matplotlib.pyplot as plt
+import matplotlib.ticker
 import numpy as np
 import seaborn as sns
 import utils 
@@ -9,15 +10,21 @@ BASELINE = "baseline"
 #BASELINE = "polyvest"
 #EXECUTABLES = ["basic-opt", "bound-remove", "fast-linalg", "vecplusextraoptim", "aligned-vec", "reduce-precision",]
 
-BRANCHES = ["polyvest", "bound-remove"] 
+#BRANCHES = ["polyvest", "bound-remove"] 
 #BRANCHES = ["polyvest", "bound-remove", "fast-linalg"] 
 #BRANCHES = ["polyvest", "bound-remove", "fast-linalg", "vecplusextraoptim"]
 #BRANCHES = ["polyvest", "bound-remove", "fast-linalg", "vecplusextraoptim", "aligned-vec"]
-#BRANCHES = ["polyvest","bound-remove", "fast-linalg", "vecplusextraoptim", "aligned-vec", "reduce-precision"]
+BRANCHES = ["polyvest","bound-remove", "fast-linalg", "vecplusextraoptim", "aligned-vec", "reduce-precision"]
+#BRANCHES = ["bound-remove", "fast-linalg", "vecplusextraoptim", "aligned-vec", "reduce-precision"]
 
 
 TEST_CASES =["cube_20", "cube_40", "cube_60", "cube_80"]
 COMPARE = "time"  # or "cycles"
+
+# Set up LaTeX and font family
+plt.rcParams['text.usetex'] = True
+plt.rc('text.latex', preamble=r'\usepackage{cmbright}')
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
 
 def get_statistic(filename, statistic_type):
     with open(filename, 'r') as f:
@@ -39,43 +46,48 @@ def get_statistic(filename, statistic_type):
 def create_plot(data, errors):
     # Seaborn style
     sns.set_style("whitegrid")
-    colors = sns.color_palette("deep", len(BRANCHES))
 
     # Number of groups and bar width
     n_groups = len(TEST_CASES)
     n_bars = len(BRANCHES)
     bar_width = 0.2  # adjust as needed
     gap_width = 0.5  # adjust as needed
-
     # Create subplots
     fig, ax = plt.subplots()
     print(data)
     # Set bar positions and plot
     for i, (exe, values) in enumerate(data.items()):
         if exe != BASELINE:
-            #ax.bar(np.arange(n_groups) * (n_bars * bar_width + gap_width) + i * bar_width, values, 
-            #       width=bar_width, label=exe, color=utils.BRANCH_COLOR_DICT[exe])
             print(exe, values)
             ax.bar(np.arange(n_groups) * (n_bars * bar_width + gap_width) + i * bar_width, values, 
                    width=bar_width, label=exe, color=utils.BRANCH_COLOR_DICT[exe], yerr=errors[exe], 
                    capsize=5, error_kw={'elinewidth':1, 'capthick':1})
 
-    # Set x-ticks in the middle of the groups of bars
-    ax.set_xticks(np.arange(n_groups) * (n_bars * bar_width + gap_width) + bar_width * (n_bars - 1) / 2)
+    
+    ax.set_xticks(np.arange(n_groups) * (n_bars * bar_width + gap_width) + bar_width * (n_bars - 1) / 2, font='serif')
     ax.set_xticklabels(TEST_CASES)
+    ax.tick_params(axis='x', labelsize=14)
+    ax.tick_params(axis='y', labelsize=14)
+    ax.xaxis.get_major_formatter()._usetex = False
+    ax.yaxis.get_major_formatter()._usetex = False
 
+    plt.text(0.0, 1, 'Speedup Ratio',
+            fontsize=14, color='k',
+            ha='left', va='bottom',
+            transform=plt.gca().transAxes)
     # Add labels, title, legend
-    ax.set_xlabel('Test Cases')
-    ax.set_ylabel('Speedup Ratio')
-    ax.set_title(f'Speedup Ratio over "{BASELINE}" ({COMPARE})', y=-0.2)
-    ax.legend()
-
+    ax.set_xlabel(r'Test Cases', fontsize=14)
+    #ax.set_ylabel(r'Speedup Ratio', fontsize=16)
+    
+    ax.set_title(r'\textbf{' + 'Speedup Ratio over "{}" ({})'.format(BASELINE, COMPARE)+'}', fontsize=14, y=1.04, loc='left', fontweight="bold")
+    ax.legend(shadow=True, fontsize=14)
     # Increase font sizes for better readability
-    plt.rcParams.update({'font.size': 14})
+    plt.rcParams.update({'font.size': 16})
 
     # Save the figure in higher resolution
-    plt.savefig(f'plots/speedup_plots/speedup_of_'+'_'.join(BRANCHES)+'_over_'+BASELINE+'_'+COMPARE+'.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'plots/speedup_plots/better_speedup_of_'+'_'.join(BRANCHES)+'_over_'+BASELINE+'_'+COMPARE+'.svg', bbox_inches='tight', dpi=300)
 
+   
 def main():
     data = {exe: [] for exe in BRANCHES}
     errors = {exe: [] for exe in BRANCHES}
